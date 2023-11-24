03aa9d05dbfd828ca4d0478005bf970c17e575f3
GoldraK <1412316-Goldrak@users.noreply.gitlab.com>
2023-11-24 13:47:13 +0000

    Update file sign.sh

--- .gitlab-ci/sign.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/expect
-
-set timeout -1
-set password ${COSIGN_PASSWORD}
-
-spawn cosign sign --key .secure_files/cosign.key -y $DOCKER_REGISTRY_URL/osdo/osdo-app@${DIGEST}
-expect "Enter password for private key:"
-send "$password\r"
-expect eof
