6bb54441c6a74618457c1053a4a1af8b48087885
GoldraK <1412316-Goldrak@users.noreply.gitlab.com>
2023-11-24 16:30:21 +0000

    Update 2 files
    - /.gitlab-ci.yml
    - /.gitlab-ci/defectdojo-finding.sh

--- .gitlab-ci/defectdojo-finding.sh
+++ .gitlab-ci/defectdojo-finding.sh
@@ -5,18 +5,3 @@ TODAY=$(date +%Y-%m-%d)
 echo ${DD_SCAN_FILE}
 cat ${DD_SCAN_FILE}
 
-curl --location --request POST "${DD_SERVER}/api/v2/import-scan/" \
-    -H 'accept: application/json' \
-    -H "Authorization: Token ${DD_API_KEY}" \
-    -F "scan_date=${TODAY}" \
-    -F "minimum_severity=${DD_SCAN_MINIMUM_SEVERITY}" \
-    -F "active=${DD_SCAN_ACTIVE}" \
-    -F "verified=${DD_SCAN_VERIFIED}" \
-    -F "scan_type=${DD_SCAN_TYPE}" \
-    -F "engagement=${DD_ENGAGEMENTID}" \
-    -F "file=@${DD_SCAN_FILE};type=application/json" \
-    -F "close_old_findings=${DD_SCAN_CLOSE_OLD_FINDINGS}" \
-    -F "environment=${DD_SCAN_ENVIRONMENT}" \
-    -F "product_name=${DD_PRODUCT_NAME}"  \
-    -F 'create_finding_groups_for_all_findings=true' \
-    -F 'group_by=component_name+component_version'
