8ffdd08487bb9b15dac7e967addc93defa270010
GoldraK <1412316-Goldrak@users.noreply.gitlab.com>
2023-12-03 16:17:39 +0100

    Add dashboard controller

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Package;
+use App\Models\PackageStats;
+use Inertia\Inertia;
+
+class DashboardController extends Controller
+{
+
+    public function show(){
+
+        $packages = Package::whereHas('versions')
+        ->with(['versions' => function ($query) {
+            $query->latest()->take(1);
+        }])
+        ->paginate(10)
+        ->through(function ($package) {
+            $latestVersion = $package->versions->first();
+            return [
+                'id' => $package->id,
+                'name' => $package->name,
+                'description' => $package->description,
+                'type' => $package->type === 1 ? 'Infrastructure' : 'CI/CD',
+                'license' => $package->license,
+                'version' => $latestVersion ? $latestVersion->version : '',
+            ];
+        });
+
+        $packageGenerate = PackageStats::count();
+      
+      return Inertia::render('Dashboard', [
+        'packages' => $packages,
+        'packageGenerate' => $packageGenerate
+      ]);
+      
+    }
+}
