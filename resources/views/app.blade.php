b97e5585d9928260a12c8b48b1db5e89b9bc71d3
GoldraK <1412316-Goldrak@users.noreply.gitlab.com>
2023-12-04 20:09:55 +0100

    Delete vue sentry

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,10 +17,5 @@
     <body class="font-sans antialiased">
         @inertia
     </body>
-    <script>
-    import * as Sentry from "@sentry/vue";
-    Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
-});
-</script>
+
 </html>
