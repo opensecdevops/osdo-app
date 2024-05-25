1711ef5c43800d6fa31b57112c4694a2ed8e2aba
Shift <shift@laravelshift.com>
2024-05-25 06:35:19 +0000

    Use Faker methods
    Accessing Faker properties was deprecated in Faker 1.14.

--- database/factories/PackageFactory.php
+++ database/factories/PackageFactory.php
@@ -20,11 +20,11 @@ class PackageFactory extends Factory
             'repository_id' => $this->faker->unique()->numberBetween(1000, 9999),
             'user_id' => \App\Models\User::factory(), // Asumiendo que tienes un modelo User y su factory
             'service_id' => \App\Models\Service::factory(), // Asumiendo que tienes un modelo Service y su factory
-            'name' => $this->faker->word,
-            'description' => $this->faker->sentence,
-            'license' => $this->faker->word,
-            'repository' => $this->faker->url,
-            'homepage' => $this->faker->url,
+            'name' => $this->faker->word(),
+            'description' => $this->faker->sentence(),
+            'license' => $this->faker->word(),
+            'repository' => $this->faker->url(),
+            'homepage' => $this->faker->url(),
             'type' => $this->faker->randomElement([1, 2]), // 1: Infra, 2: Pipeline
             'message' => 'pending', // Valor predeterminado
             'status' => 0, // Valor predeterminado
